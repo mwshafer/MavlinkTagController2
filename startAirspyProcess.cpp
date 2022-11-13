c2901d5048a38fcc9ba69398a788193e3056db0f
Don Gagne <don@thegagnes.com>
2022-11-13 15:17:01 -0800

    Capture data dir

--- startAirspyProcess.cpp
+++ startAirspyProcess.cpp
@@ -10,10 +10,10 @@
 
 void startAirspyHF(void)
 {
-   	std::system("./airspyhf_rx -f 146 -m on -a 192000 -g on -l high -t 0 -r /data/airspy_hf.dat -n 5760000");
+   	std::system("./airspyhf_rx -f 146 -m on -a 192000 -g on -l high -t 0 -r /home/pi/data/airspy_hf.dat -n 5760000");
 }
 
 void startAirspyMini(void)
 {
-   	std::system("airspy_rx -r /data/airspy_mini.dat -f 146 -a 3000000 -h 21 -t 0 -n 90000000");
+   	std::system("airspy_rx -r /home/pi/data/airspy_mini.dat -f 146 -a 3000000 -h 21 -t 0 -n 90000000");
 }
