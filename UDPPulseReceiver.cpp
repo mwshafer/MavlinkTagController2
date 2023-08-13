10085d009dadb43fbe801e83354d484dd0fd434d
Don Gagne <don@thegagnes.com>
2023-08-13 11:53:26 -0700

    Fix TelemetryCache lookups

--- UDPPulseReceiver.cpp
+++ UDPPulseReceiver.cpp
@@ -119,40 +119,41 @@ void UDPPulseReceiver::_receive()
 
             memset(&pulseInfo, 0, sizeof(pulseInfo));
 
-            auto telemetry = _telemetryCache->telemetryForTime(udpPulseInfo.start_time_seconds);
-
             pulseInfo.header.command                = COMMAND_ID_PULSE;
             pulseInfo.tag_id                        = (uint32_t)udpPulseInfo.tag_id;
             pulseInfo.frequency_hz                  = (uint32_t)udpPulseInfo.frequency_hz;
-            pulseInfo.start_time_seconds            = udpPulseInfo.start_time_seconds;
-            pulseInfo.predict_next_start_seconds    = udpPulseInfo.predict_next_start_seconds;
-            pulseInfo.snr                           = udpPulseInfo.snr;
-            pulseInfo.stft_score                    = udpPulseInfo.stft_score;
-            pulseInfo.group_seq_counter             = (uint16_t)udpPulseInfo.group_seq_counter;
-            pulseInfo.group_ind                     = (uint16_t)udpPulseInfo.group_ind;
-            pulseInfo.group_snr                     = udpPulseInfo.group_snr;
-            pulseInfo.detection_status              = (uint8_t)udpPulseInfo.detection_status;
-            pulseInfo.confirmed_status              = (uint8_t)udpPulseInfo.confirmed_status;
-            pulseInfo.position_x                    = telemetry.position.latitude;
-            pulseInfo.position_y                    = telemetry.position.longitude;
-            pulseInfo.position_z                    = telemetry.position.relativeAltitude;
-            pulseInfo.orientation_x                 = telemetry.attitudeQuaternion.x;
-            pulseInfo.orientation_y                 = telemetry.attitudeQuaternion.y;
-            pulseInfo.orientation_z                 = telemetry.attitudeQuaternion.z;
-            pulseInfo.orientation_w                 = telemetry.attitudeQuaternion.w;
-            pulseInfo.noise_psd                     = udpPulseInfo.noise_psd;
 
             if (pulseInfo.frequency_hz == 0) {
                 logInfo() << "HEARTBEAT from Detector" << pulseInfo.tag_id;
             } else {
-                std::string pulseStatus = formatString("Conf: %u Id: %2u stft_score: %5.1f snr: %5.1f noise_psd: %5.1g seq_counter: %8u freq: %9u yaw/alt: %4.0f/%3.0f",
+                auto telemetry = _telemetryCache->telemetryForTime(udpPulseInfo.start_time_seconds);
+
+                pulseInfo.start_time_seconds            = udpPulseInfo.start_time_seconds;
+                pulseInfo.predict_next_start_seconds    = udpPulseInfo.predict_next_start_seconds;
+                pulseInfo.snr                           = udpPulseInfo.snr;
+                pulseInfo.stft_score                    = udpPulseInfo.stft_score;
+                pulseInfo.group_seq_counter             = (uint16_t)udpPulseInfo.group_seq_counter;
+                pulseInfo.group_ind                     = (uint16_t)udpPulseInfo.group_ind;
+                pulseInfo.group_snr                     = udpPulseInfo.group_snr;
+                pulseInfo.detection_status              = (uint8_t)udpPulseInfo.detection_status;
+                pulseInfo.confirmed_status              = (uint8_t)udpPulseInfo.confirmed_status;
+                pulseInfo.position_x                    = telemetry.position.latitude;
+                pulseInfo.position_y                    = telemetry.position.longitude;
+                pulseInfo.position_z                    = telemetry.position.relativeAltitude;
+                pulseInfo.orientation_x                 = telemetry.attitudeQuaternion.x;
+                pulseInfo.orientation_y                 = telemetry.attitudeQuaternion.y;
+                pulseInfo.orientation_z                 = telemetry.attitudeQuaternion.z;
+                pulseInfo.orientation_w                 = telemetry.attitudeQuaternion.w;
+                pulseInfo.noise_psd                     = udpPulseInfo.noise_psd;
+
+                std::string pulseStatus = formatString("Conf: %u Id: %2u snr: %5.1f noise_psd: %5.1g freq: %9u lat/lon/yaw/alt: %3.6f/%3.6f/%4.0f/%3.0f",
                                                 pulseInfo.confirmed_status,
                                                 pulseInfo.tag_id,
-                                                pulseInfo.stft_score,
                                                 pulseInfo.snr,
                                                 pulseInfo.noise_psd,
-                                                pulseInfo.group_seq_counter,
                                                 pulseInfo.frequency_hz,
+                                                telemetry.position.latitude,
+                                                telemetry.position.longitude,
                                                 telemetry.attitudeEuler.yawDegrees,
                                                 telemetry.position.relativeAltitude);
                 if (udpPulseInfo.confirmed_status) {
