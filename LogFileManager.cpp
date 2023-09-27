3f87b014749b584067970a0895aae1fed87c4a80
Don Gagne <don@thegagnes.com>
2023-09-27 17:54:27 +0000

    Keep 10 flight log runs

--- LogFileManager.cpp
+++ LogFileManager.cpp
@@ -1,10 +1,16 @@
 #include "LogFileManager.h"
 #include "formatString.h"
+#include "log.h"
 
 #include <stdlib.h>
-#include <filesystem>
+#include <boost/filesystem.hpp>
+#include <boost/system/error_code.hpp>
 
-LogFileManager* LogFileManager::_instance = nullptr;
+namespace bf = boost::filesystem;
+namespace bs = boost::system;
+
+LogFileManager* LogFileManager::_instance       = nullptr;
+const char* 	LogFileManager::_logDirPrefix   = "Logs";
 
 LogFileManager* LogFileManager::instance()
 {
@@ -16,14 +22,22 @@ LogFileManager* LogFileManager::instance()
 
 LogFileManager::LogFileManager()
 {
-    _logDir = std::string(getenv("HOME")) + "/Logs";
+    _homeDir    = std::string(getenv("HOME"));
+    _logDir     = formatString("%s/%s_1", _homeDir.c_str(), _logDirPrefix);
 }
 
 void LogFileManager::detectorsStarted()
 {
     if (_detectorStartIndex++ == 0) {
-        std::filesystem::remove_all(_logDir);
-        std::filesystem::create_directory(_logDir);
+        bs::error_code errorCode;
+        
+        for (int i=10; i>=1; i--) {
+            auto oldName = formatString("%s/%s_%d", _homeDir.c_str(), _logDirPrefix, i);
+            auto newName = formatString("%s/%s_%d", _homeDir.c_str(), _logDirPrefix, i + 1);
+            bf::rename(oldName.c_str(), newName.c_str(), errorCode);
+        }
+
+        bf::create_directory(_logDir.c_str(), errorCode);
     }
 }
 
