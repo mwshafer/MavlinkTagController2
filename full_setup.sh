7677e9b7c7414ac2b7add085830b842d5023960b
Don Gagne <DonLakeFlyer@users.noreply.github.com>
2023-10-05 10:04:33 -0700

    Update full_setup.sh

--- full_setup.sh
+++ full_setup.sh
@@ -15,13 +15,12 @@ cd ~/repos
 
 echo "*** Clone and build MavlinkTagController"
 cd ~/repos
-if [ ! -d MavlinkTagController ]; then
-	git clone --recursive git@github.com:DonLakeFlyer/MavlinkTagController.git
+if [ ! -d MavlinkTagController2 ]; then
+	git clone --recursive git@github.com:DonLakeFlyer/MavlinkTagController2.git
 fi
-cd ~/repos/MavlinkTagController
+cd ~/repos/MavlinkTagController2
 git pull origin main
-cmake -Bbuild -H. 
-cmake --build build -j4
+make
 
 echo "*** Clone and build csdr-uavrt"
 cd ~/repos
@@ -36,17 +35,17 @@ sudo make install
 echo "*** Clone and build airspy_channelize"
 cd ~/repos
 if [ ! -d airspy_channelize ]; then
-	git clone --recursive -b don-codegen git@github.com:dynamic-and-active-systems-lab/airspy_channelize.git
+	git clone --recursive git@github.com:dynamic-and-active-systems-lab/airspy_channelize.git
 fi
 cd ~/repos/airspy_channelize
-git pull origin don-codegen
-make -f matlab-coder-utils/Makefile PRODUCT_NAME=airspy_channelize -j4
+git pull origin main
+make
 
 echo "*** Clone and build uavrt_detection"
 cd ~/repos
 if [ ! -d uavrt_detection ]; then
-	git clone --recursive -b don-codegen git@github.com:dynamic-and-active-systems-lab/uavrt_detection.git
+	git clone --recursive git@github.com:dynamic-and-active-systems-lab/uavrt_detection.git
 fi
 cd ~/repos/uavrt_detection
 git pull origin don-codegen
-make -f matlab-coder-utils/Makefile PRODUCT_NAME=uavrt_detection -j4
+make
